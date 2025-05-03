74df7365e194144dde8e7fb547efdabca39c8edf
Verticraze <verticraze@outlook.com>
2025-05-03 17:11:26 +0530

    Added error handling methods

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
-#include<unistd.h>
+//includes
+#include<ctype.h>
+#include<errno.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include<ctype.h>
 #include<termios.h>
+#include<unistd.h>
+
+#define CTRL_KEY(k) ((k)&0x1f)
+
+//data
 struct termios orig_termios;
+//terminal
+void die(const char *s)
+{
+	perror(s);
+	exit(1);
+}
 void disableRawMode()
 {
-	tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+	if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)== -1)
+	{
+		die("tcsetattr");
+	}
 }
 void enableRawMode()
 {
-	tcgetattr(STDIN_FILENO,&orig_termios);
+	if(tcgetattr(STDIN_FILENO,&orig_termios)== -1)
+	{
+		die("tcgetattr");
+	}
 	atexit(disableRawMode);
 	
 	struct termios raw=orig_termios;
@@ -18,31 +36,46 @@ void enableRawMode()
 	raw.c_oflag&=OPOST;
 	raw.c_cflag|=(CS8);
 	raw.c_lflag &= ~( ECHO | ICANON| IEXTEN | ISIG);
-	tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
 	raw.c_cc[VMIN]=0;
-	raw.c_cc[VTIME]=10;
+	raw.c_cc[VTIME]=1;
 	
-	tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+	if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)== -1)
+	{
+		die("tcsetattr");
+	}
 }
+
+char editorReadKey()
+{
+		int nread;
+		char c;
+		while((nread=read(STDIN_FILENO,&c,1))!=1)
+		{
+			if(nread== -1 && errno!=EAGAIN)
+			{
+				die("read");
+			}
+		}
+		return c;
+}
+//input
+void editorProcessKeyPress()
+{
+	char c =editorReadKey();
+	
+	switch(c)
+	{
+		case CTRL_KEY('q'):exit(0);
+							break;
+	}
+}
+//init
 int main()
 {
 	enableRawMode();
 	while(1)
 	{
-		char c='\0';
-		read(STDIN_FILENO,&c,1);
-		if(iscntrl(c))
-		{
-			printf("%d\r\n",c);
-		}
-		else
-		{
-			printf("%d('%c')\r\n",c,c);
-		}
-		if(c =='q')
-		{
-			break;
-		}
+		editorProcessKeyPress();
 	}
 
 	return 0;
