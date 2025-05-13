39ae8735df8800aaa4feb8e2492ba161fdba269f
Verticraze <verticraze@outlook.com>
2025-05-13 17:55:05 +0530

    Fixed cursor positioning
    Fixed cursor positioning, and added the ability to scroll past the right edge of the screen

--- main.c
+++ main.c
@@ -516,7 +516,7 @@ void editordrawRows(struct abuf *ab)
 				len = E.screencols;
 			}
 			
-			abAppend(ab, E.row[filerows].chars[E.coloff],len);
+			abAppend(ab, &E.row[filerows].chars[E.coloff],len);
 				
 		}
 
@@ -547,7 +547,7 @@ void editorRefreshScreen()
 
     char buf[32];
 
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
 
     abAppend(&ab, buf, strlen(buf));
 
@@ -589,10 +589,7 @@ void editorMoveCursor(int key)
             break;
 
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1)
-            {
-                E.cx++;
-            }
+             E.cx++;
             break;
 
     }
